53848036d7fa38a36c9673e3dd14c7452a3d68a0
franciscocruz29 <franciscohuertas25@gmail.com>
2023-01-05 10:26:57 -0500

    Mode and styling setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,35 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode); // grab information from the store
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); // The useMemo Hook can be used to keep expensive, resource intensive functions from needlessly running.
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route>
-            <Route path="/" element={<LoginPage />} />
-            <Route
-              path="/home"
-              element={<HomePage />}
-            />
-            <Route
-              path="/profile/:userId"
-              element={<ProfilePage />}
-            />
-          </Route>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+            <Routes>
+              <Route>
+                <Route path="/" element={<LoginPage />} />
+                <Route
+                  path="/home"
+                  element={<HomePage />}
+                />
+                <Route
+                  path="/profile/:userId"
+                  element={<ProfilePage />}
+                />
+              </Route>
+            </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
