44935aa330b0ed4ab5ae289ef30204c839a101a8
franciscocruz29 <franciscohuertas25@gmail.com>
2023-01-11 09:20:59 -0500

    Created advert widget

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -4,6 +4,7 @@ import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
 
 
 const HomePage = () => {
@@ -32,6 +33,7 @@ const HomePage = () => {
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
+            <AdvertWidget />
             <Box m="2rem 0" />
           </Box>
         )}
